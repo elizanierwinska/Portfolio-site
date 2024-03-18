6006ec2d31b8bad8cd76c2143c938b6ca108a513
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-18 16:00:45 +0100

    Setting up the base of the webpage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 id="name">Eliza Nierwińska</h1>
+        <p id="title">Junior Frontend Developer</p>
+        <a>Projects</a>
+        <a >Resume</a>
+        <a>Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
