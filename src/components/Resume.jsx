b83ed7bbd6f1484ae2917cb7d0a8c3a53ff1bdfa
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-19 17:04:45 +0100

    Add elements to the resume, style it

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -12,27 +12,66 @@ function Resume() {
         </Link>
       </div>
       <div className="resume">
-        <div>
+        <div className="resume-header">
           <h1>Summary</h1>
           <hr/>
         </div>
-        <p>As a self-taught frontend developer, I have passion for creating visually appealing and user-friendly web applications. With a strong grasp of HTML, CSS, JavaScript, and React, I am adept at translating design concepts into responsive web interfaces. I want to contribute to the success of a forward-thinking company and team with a problem-solving attitude so as to further develop my skills and expertise.</p>
-        <div>
+        <p>As a self-taught frontend developer, I am passionate about creating visually appealing and user-friendly web applications. With a strong grasp of HTML, CSS, JavaScript, and React, I am adept at translating design concepts into responsive web interfaces. I want to contribute to the success of a forward-thinking company and team with a problem-solving attitude so as to further develop my skills and expertise.</p>
+        <div className="resume-header">
           <h1>Skills</h1>
           <hr />
         </div>
-        <div>
+        <div className="flex first">
+          <p className="bold-p">Programming languages:</p>
+          <p>JavaScript, TypeScript, Python, HTML, CSS</p>
+        </div>
+        <div className="flex">
+          <p className="bold-p">Frameworks/Libraries:</p>
+          <p>React, Three.js, Next.js, Redux, Tailwind, Bulma</p>
+        </div>
+        <div className="flex">
+          <p className="bold-p">Tools:</p>
+          <p>Git, Command Line, Microsoft Office</p>
+        </div>
+        <div className="resume-header">
           <h1>Education</h1>
           <hr />
         </div>
-        <div>
+        <div className="education-and-experience">
+          <h2><b>University of Warmia and Mazury</b> / B.A. Linguistics in Business</h2>
+          <p className="gpa"><b>GPA: </b>4.5/5.0; <b>Thesis: </b>5.0/5.0</p>
+          <p className="date">October 2020 - June 2023, Olsztyn</p>
+          <ul>
+            <li>Academic focus: English, German, Economics, IT, and Accounting</li>
+            <li>Professional programs with Citibank Olsztyn, Transcom Worldwide Poland
+            in Poznań, and Poznań International Fair</li>
+          </ul>
+          <h2><b>Zespół Szkół Weterynaryjnych i Ogólnokształcących nr 7 w Łomży</b></h2>
+          <h3>Technical College / Landscape Architecture</h3>
+          <p className="date">September 2016 - May 2020</p>
+          <ul>
+            <li>User-centric design, spatial awareness, research skills, client interaction</li>
+          </ul>
+        </div>
+        <div className="resume-header">
           <h1>Professional Experience</h1>
           <hr />
         </div>
-        <div>
+        <div className="education-and-experience">
+          <h2><b>Biotikur</b> / Business Analytics and Marketing Intern</h2>
+          <p class="company-description"><i>Biotikur is an applied microbiome research UG based in Berlin, Germany</i></p>
+          <p className="date">July 2022 - August 2022, Berlin</p>
+          <ul>
+            <li>Research and identify influential social media influencers for collaboration</li>
+            <li>Examine competitive landscape to create new social media strategies</li>
+            <li>Analyze sales website performance (SEO, ad conversion) and tweak CSS/HTML design</li>
+          </ul>
+        </div>
+        <div className="resume-header">
           <h1>Languages</h1>
           <hr />
         </div>
+        <p>English (fluent), Polish (fluent), German (B1)</p>
       </div>
       <Footer />
     </div>
