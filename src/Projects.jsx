8b9e05ba5f141b678a9665af9057f7160e382885
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-19 10:37:44 +0100

    style: add go bac to the main page button

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,18 @@
 import './Projects.css';
+import { Link } from 'react-router-dom';
 
 function Projects() {
   return (<div className="projects">
+    <div>
+      <Link id="back-button" to="/">
+        <p id="arrow">&#8592;</p>
+        <p>back</p>
+      </Link>
+    </div>
     <div className="project-description">
-      <img src="../images/3d-portfolio.png" alt="Portfolio site made with Three.js"/>
+      <Link to={"https://portfolio-three-js-dromuarga.vercel.app/"}>
+        <img src="../images/3d-portfolio.png" alt="Portfolio site made with Three.js"/>
+      </Link>
       <div className="description">
         <h3>Portfolio site</h3>
         <h4>Made with:</h4>
@@ -14,7 +23,37 @@ function Projects() {
       </div>
     </div>
     <div className="project-description">
-      <img src="../images/marble-race.png" alt="Marble race game made with Three.js"/>
+      <Link to={"https://calculator-en.vercel.app/"}>
+        <img src="../images/calculator.png" alt="Calculator"/>
+      </Link>
+      <div className="description">
+        <h3>Calculator</h3>
+        <h4>Made with:</h4>
+        <div className="tags">
+          <p>JavaScript</p>
+          <p>HTML</p>
+          <p>CSS</p>
+        </div>
+      </div>
+    </div>
+    <div className="project-description">
+      <Link to={"https://to-do-list-en.vercel.app/"}>
+        <img src="../images/to-do-list.png" alt="To-do list"/>
+      </Link>
+      <div className="description">
+        <h3>Calculator</h3>
+        <h4>Made with:</h4>
+        <div className="tags">
+          <p>JavaScript</p>
+          <p>HTML</p>
+          <p>CSS</p>
+        </div>
+      </div>
+    </div>
+    <div className="project-description">
+      <Link to={"https://marble-race-game-en.vercel.app/"}>
+        <img src="../images/marble-race.png" alt="Marble race game made with Three.js"/>
+      </Link>
       <div className="description">
         <h3>Marble race game</h3>
         <h4>Made with:</h4>
@@ -25,37 +64,15 @@ function Projects() {
       </div>
     </div>
     <div className="project-description">
-      <img src="../images/portal-scene.png" alt="Portal scene made with Three.js"/>
+      <Link to={"https://portal-dromuarga.vercel.app/"}>
+        <img src="../images/portal-scene.png" alt="Portal scene made with Three.js"/>
+      </Link>
       <div className="description">
         <h3>Portal scene</h3>
         <h4>Made with:</h4>
         <div className="tags">
           <p>Three.js</p>
           <p>Blender</p>
-        </div>
-      </div>
-    </div>
-    <div className="project-description">
-      <img src="../images/calculator.png" alt="Calculator"/>
-      <div className="description">
-        <h3>Calculator</h3>
-        <h4>Made with:</h4>
-        <div className="tags">
-          <p>JavaScript</p>
-          <p>HTML</p>
-          <p>CSS</p>
-        </div>
-      </div>
-    </div>
-    <div className="project-description">
-      <img src="../images/to-do-list.png" alt="To-do list"/>
-      <div className="description">
-        <h3>Calculator</h3>
-        <h4>Made with:</h4>
-        <div className="tags">
-          <p>JavaScript</p>
-          <p>HTML</p>
-          <p>CSS</p>
         </div>
       </div>
     </div>
