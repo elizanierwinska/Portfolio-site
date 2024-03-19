ca2b0ae4bd19d48a963f4757841d8908148f1ec9
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-19 14:37:16 +0100

    style: work on placement of element and hover effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,25 +4,29 @@ import Footer from './Footer';
 
 function Contact() {
   return (<div id="contact">
-    <div>
+    <div className="back-button-container">
       <Link id="back-button" to="/">
         <p id="arrow">&#8592;</p>
         <p>back</p>
       </Link>
     </div>
-    <p id="title">You can check my channels below:</p>
-    <Link to="https://www.linkedin.com/in/eliza-nierwi%C5%84ska-583385278/">
-      <img src="../images/linkedin_logo_icon.png" alt=""/>
-    </Link>
-    <Link>
-      <img src="../images/github_logo.png" alt=""/>
-    </Link>
-    <Link>
-      <img src="../images/blog-icon.png" alt=""/>
-    </Link>
-    <Link>
-      <img src="../images/email-icon.png" alt=""/>
-    </Link>
+    <div className="contact-container">
+      <p id="title">You can check my channels below:</p>
+      <div className="contact-info">
+        <Link to="https://www.linkedin.com/in/eliza-nierwi%C5%84ska-583385278/">
+          <img src="../images/linkedin_logo_icon.png" alt=""/>
+        </Link>
+        <Link to="https://github.com/elizanierwinska">
+          <img src="../images/github_logo.png" alt=""/>
+        </Link>
+        <Link to="https://tech-blog-dromuarga.vercel.app/">
+          <img src="../images/blog-icon.png" alt=""/>
+        </Link>
+        <Link to="mailto:nierwinskaeliza@gmail.com">
+          <img src="../images/email-icon.png" alt=""/>
+        </Link>
+      </div>
+    </div>
     <Footer/>
   </div>)
 }
